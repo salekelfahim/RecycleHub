ea8a242779622c97dd48147c29de347132fae382
salekelfahim <salekelfahim@gmail.com>
2025-02-04 17:10:00 +0100

    adding register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import {LandingComponent} from "./components/landing/landing.component";
+import {RegisterComponent} from "./components/auth/register/register.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: LandingComponent,
     title: 'RecycleHub - Transform Your Waste into Rewards'
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'RecycleHub - Register'
   },
 ];
