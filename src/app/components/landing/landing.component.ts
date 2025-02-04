6b646b59cd40ede1a49256b5af6641442fb8d5df
salekelfahim <salekelfahim@gmail.com>
2025-02-04 12:30:47 +0100

    adding landing page

---
+++ src/app/components/landing/landing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent {
+}
