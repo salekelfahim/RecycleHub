0be7f417225cad19732ded1beb09922e34f7964d
salekelfahim <salekelfahim@gmail.com>
2025-02-06 12:25:54 +0100

    adding the particulier component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 import {LandingComponent} from "./components/landing/landing.component";
 import {RegisterComponent} from "./components/auth/register/register.component";
 import {LoginComponent} from "./components/auth/login/login.component";
+import {
+  ParticulierDashboardComponent
+} from "./components/particulier/particulier-dashboard/particulier-dashboard.component";
+import {MyPostsComponent} from "./components/particulier/my-posts/my-posts.component";
 
 export const routes: Routes = [
   {
@@ -18,5 +22,12 @@ export const routes: Routes = [
     path: 'login',
     component: LoginComponent,
     title: 'RecycleHub - Login'
+  },
+  {
+    path: 'dashboard',
+    component: ParticulierDashboardComponent,
+    children: [
+      { path: 'posts', component: MyPostsComponent },
+    ]
   },
 ];
